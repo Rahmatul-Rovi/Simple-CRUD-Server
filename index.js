2319d9d1c2551cb19e1d419ae9812b9286ac1b6c
Rahmatul Rovi <rahmatul.rovi@gmail.com>
2026-01-05 21:28:09 +0600

    Mongo Section added

--- index.js
+++ index.js
@@ -1,13 +1,32 @@
 const express = require('express');
 const app = express();
-
+const { MongoClient, ServerApiVersion} = require('mongodb');
 const cors = require('cors');
 const port = process.env.PORT ||3000; 
 
 
 //User: SimpleDBUser
 //Password:XLMo3CWnjiegYnFj
+const uri = "mongodb+srv://SimpleDBUser:XLMo3CWnjiegYnFj@cluster0.bou0ahg.mongodb.net/?appName=Cluster0";
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+async function run(){
+     try{
+      await client.connect();
+      await client.db('admin').command({ping:1});
+      console.log("Pinged your deployment. You successfully connected to MongoDB!");
+     }
+     finally{
 
+     }
+}
+run().catch(console.dir);
 
 //middleWare
 app.use(cors());
